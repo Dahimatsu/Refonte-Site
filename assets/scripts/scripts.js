53389c477b651843560026da351c57acb0fab48d
Tony Mahefa <rtonymahefa@gmail.com>
2026-02-21 22:07:32 +0300

    Header floating

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -7,3 +7,13 @@ window.addEventListener('load', () => {
         document.body.classList.remove('no-scroll');
     }, 3000);
 });
+
+const header = document.getElementById('main-header');
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 50) {
+        header.classList.add('header-scrolled');
+    } else {
+        header.classList.remove('header-scrolled');
+    }
+});
