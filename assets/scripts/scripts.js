1a692b045d2cf99f6c9fa33b44afafd6160c2493
Tony Mahefa <rtonymahefa@gmail.com>
2026-02-23 11:20:17 +0300

    llien page

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -201,12 +201,12 @@ window.addEventListener('scroll', () => {
         header.classList.add('header-text-light');
         header.classList.remove('header-text-dark');
 
-        navbarLogo.src = 'assets/images/logo_semi-negatif.png';
+        navbarLogo.src = '../assets/images/logo_semi-negatif.png';
     } else {
         header.classList.add('header-text-dark');
         header.classList.remove('header-text-light');
 
-        navbarLogo.src = 'assets/images/logo_positif.png';
+        navbarLogo.src = '../assets/images/logo_positif.png';
     }
 });
 
